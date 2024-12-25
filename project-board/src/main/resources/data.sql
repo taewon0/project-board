43913b98134105ce65788c22a11ea30423f7c90a
taewon0 <ktw495@naver.com>
2024-12-25 15:31:40 +0900

    feat: 테스트 데이터 data.sql
    jpa 레포지토리에 사용되는 테스트 데이터

---
+++ project-board/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into article(title, content, hashtag, created_by, modified_by, created_at, modified_at) values
+('java', 'what is difference with version 17 and 11?', '#java', 'faker', 'faker', '2021-05-23 08:34:54', '2021-05-23 08:35:54'),
+('red', 'you and i go hard at each other like we going to war.', '#java', 'code', 'blame', '2022-05-23 08:34:54', '2022-05-23 08:35:54'),
+('git', 'i cannot tell you what it really is.', '#sing', 'faster', 'github', '2023-05-23 08:34:54', '2023-05-23 08:35:54'),
+('fast', 'try to tell you no', '#sing', 'project', 'online', '2023-05-25 08:34:54', '2023-06-23 08:35:54'),
+('ppp', 'my mind keep saying run as fast as you can', '#sing', 'lecture', 'school', '2021-05-23 08:34:54', '2024-05-23 08:35:54'),
+('note', 'samsung note book galaxy cool', '#main', 'car', 'terry', '2021-05-23 08:34:54', '2021-05-28 08:35:54'),
+('book', 'kyobo book market aladin yes24', '#main', 'alex', 'john', '2020-05-23 08:34:54', '2021-05-23 08:35:54'),
+('table', 'furniture how are you cafe', '#main', 'james', 'alex', '2020-05-23 08:34:54', '2022-05-23 08:35:54'),
+('chair', 'chair shot wwe champion', '#test', 'alex', 'alex', '2021-08-23 08:34:54', '2021-10-23 08:35:54'),
+('travel', 'japan america england china korea vietnam', '#trip', 'faker', 'faker', '2021-05-23 08:34:54', '2021-05-23 08:40:54'),
+('soccer', 'messi ronaldo very nice player in the world soccer', '#soccer', 'salah', 'salah', '2021-05-23 08:34:54', '2021-05-24 08:35:54');
