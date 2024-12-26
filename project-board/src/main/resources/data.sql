81b12e9b023b6aca4a83c60faab4b521a2bfd9eb
taewon0 <ktw495@naver.com>
2024-12-26 12:55:33 +0900

    feat: article_comment 테스트 데이터 추가
    cascading 확인을 위한 article_comment 테스트 데이터를 추가하였다.

--- project-board/src/main/resources/data.sql
+++ project-board/src/main/resources/data.sql
@@ -10,3 +10,16 @@ insert into article(title, content, hashtag, created_by, modified_by, created_at
 ('chair', 'chair shot wwe champion', '#test', 'alex', 'alex', '2021-08-23 08:34:54', '2021-10-23 08:35:54'),
 ('travel', 'japan america england china korea vietnam', '#trip', 'faker', 'faker', '2021-05-23 08:34:54', '2021-05-23 08:40:54'),
 ('soccer', 'messi ronaldo very nice player in the world soccer', '#soccer', 'salah', 'salah', '2021-05-23 08:34:54', '2021-05-24 08:35:54');
+
+insert into article_comment(article_id, content, created_at, modified_at, created_by, modified_by) values
+(1, 'this is content', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(1, 'study hate', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'bmen', 'bmen'),
+(1, 'hello mr ms', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'cmen', 'cmen'),
+(2, 'client server', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(2, 'tcp socket udp', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(3, 'layer many much a lot', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(4, 'data path application', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(5, 'why so sirius', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(5, 'papago google alphago', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(5, 'naver never ever', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen'),
+(5, 'samsung lg sk hanhwa hyundai', '2023-12-12 08:34:23', '2023-12-12 08:34:23', 'amen', 'amen');
