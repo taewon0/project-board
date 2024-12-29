7f1ede249f9b445afa26ea546ad4af1c70c8cb16
taewon0 <ktw495@naver.com>
2024-12-29 14:40:28 +0900

    feat: 모든 테스트의 미디어타입 비교 부분 수정
    contentTypeCompatibleWith로 변경해서 부가적인 옵션이 붙어도 통과하게 변경

--- project-board/src/test/java/com/tw/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/tw/projectboard/controller/ArticleControllerTest.java
@@ -43,7 +43,7 @@ class ArticleControllerTest {
         //when&then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -59,7 +59,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Disabled("구현 중")
@@ -72,6 +72,6 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
