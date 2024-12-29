e40a609b8f17c026ac29d0a46092d4db7a5b039d
taewon0 <ktw495@naver.com>
2024-12-29 16:45:28 +0900

    feat: 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한번에 적용

--- project-board/src/main/java/com/tw/projectboard/controller/ArticleController.java
+++ project-board/src/main/java/com/tw/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.tw.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,6 +16,13 @@ public class ArticleController {
     public String articles(ModelMap map){
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String articles(@PathVariable(name = "articleId") Long articleId, ModelMap map){
+        map.addAttribute("article", "article"); //TODO: 구현할 때 실제 데이터 넣어야함
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 
 }
