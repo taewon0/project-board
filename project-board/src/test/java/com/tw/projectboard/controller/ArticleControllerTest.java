e207215339dc6f3699c40f6611f017cb3b9dec8b
taewon0 <ktw495@naver.com>
2024-12-29 14:25:41 +0900

    feat: 게시판 뷰 골격 작성
    게시판 기본 뷰인 index.html 작성
    컨트롤러 게시판 리스트 조회 테스트 통과하게 열어줌
    테스트에서 미디어 타입에 부가적인 옵션이 들어간 경우를 위해 compatibleWIth로 변경

--- project-board/src/test/java/com/tw/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/tw/projectboard/controller/ArticleControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
-@WebMvcTest(ArticleControllerTest.class) //(클래스)를 넣어주면 해당 테스트 클래스만 테스트함
+@WebMvcTest(ArticleController.class) //(클래스)를 넣어주면 해당 클래스만 테스트함
 class ArticleControllerTest {
 
     private final MockMvc mvc;
@@ -21,7 +21,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -30,7 +29,7 @@ class ArticleControllerTest {
         //when&then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
