0367cfacf44ccd5c101df664152fd3e5873a77f8
taewon0 <ktw495@naver.com>
2025-01-04 16:54:43 +0900

    feat: DTO 재설계

--- project-board/src/main/java/com/tw/projectboard/dto/ArticleCommentDto.java
+++ project-board/src/main/java/com/tw/projectboard/dto/ArticleCommentDto.java
@@ -1,18 +1,46 @@
 package com.tw.projectboard.dto;
 
+import com.tw.projectboard.domain.Article;
+import com.tw.projectboard.domain.ArticleComment;
+
 import java.time.LocalDateTime;
 
 /**
  * DTO for {@link com.tw.projectboard.domain.ArticleComment}
  */
 public record ArticleCommentDto(
+        Long id,
+        Long articleId,
+        UserAccountDto userAccountDto,
         String content,
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static ArticleCommentDto of(String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new ArticleCommentDto(content, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
     }
+
+    public static ArticleCommentDto from(ArticleComment entity) {
+        return new ArticleCommentDto(
+                entity.getId(),
+                entity.getArticle().getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getContent(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public ArticleComment toEntity(Article entity) {
+        return ArticleComment.of(
+                entity,
+                userAccountDto.toEntity(),
+                content
+        );
+    }
+
 }
